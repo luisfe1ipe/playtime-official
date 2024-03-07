d60fb00fd6d65abe0f23dfdde1e02a5409aa4aa3
Luis Felipe <108816416+luisfe1ipe@users.noreply.github.com>
2024-03-07 11:25:28 -0300

    fix

--- resources/views/livewire/find-player/select-game.blade.php
+++ resources/views/livewire/find-player/select-game.blade.php
@@ -4,7 +4,7 @@
         <p class="my-4 text-lg">
             Por favor, escolha o jogo que você deseja jogar antes de prosseguir para encontrar um player.
         </p>
-        {{-- <div class="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"> --}}
+        <div class="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
             @foreach ($games as $game)
             <x-card-game :game="$game" />
             @endforeach
