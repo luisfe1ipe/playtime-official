fbea6ff69102caa462ce96a25e8b76138d74d7e0
Luis Felipe <108816416+luisfe1ipe@users.noreply.github.com>
2024-03-12 14:41:15 -0300

    fix: mudando cor da borda ao existir erro

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,6 +1,6 @@
 <button {{$attributes->merge(['type' => 'submit', 'class' => 'flex gap-1 items-center focus:outline-none focus:ring-4
   font-medium rounded-lg
-  text-sm px-5 py-2.5 bg-purple-600 hover:bg-purple-700
-  focus:ring-purple-900'])}}>
+  text-sm px-5 py-2.5 bg-primary-600 hover:bg-primary-700
+  focus:ring-primary-900'])}}>
   {{$slot}}
 </button>
--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,9 +1,10 @@
 @props(['disabled' => false, 'error' => null])
 
 @php
-$errorClass = $error ? 'border-red-400 text-red-400' : '';
+$errorClass = $error ? 'border-red-400 text-red-400' : 'border-zinc-700';
 @endphp
 
 <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'block w-full rounded-lg shadow-sm
-outline-none transition duration-75 focus:ring-1 focus:ring-inset disabled:opacity-70 bg-zinc-900 focus:ring-primary-500
-border-zinc-700 focus:border-primary-500 ' . $errorClass]) !!}>
+outline-none transition duration-75 border focus:ring-1 focus:ring-inset disabled:opacity-70 bg-zinc-900
+focus:ring-primary-500
+ focus:border-primary-500 ' . $errorClass]) !!} >
