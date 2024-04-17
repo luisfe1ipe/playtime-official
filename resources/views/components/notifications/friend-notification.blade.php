28fc0afa123f8c44ab545b81db9ac39e5e1e3965
Luis Felipe <108816416+luisfe1ipe@users.noreply.github.com>
2024-04-17 12:47:59 -0300

    feat: criando componente de notificação para a notificação de amizade

--- resources/views/components/notifications/friend-notification.blade.php
+++ resources/views/components/notifications/friend-notification.blade.php
@@ -3,14 +3,21 @@
 <div wire:key="{{ $notification->id }}"
     class="flex items-center w-full gap-4 p-2 transition-colors ease-linear rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-zinc-950/80">
     <div class="flex w-full gap-1">
-        <img class="rounded-full size-10" src="{{ $notification->data['user_registered']['photo'] }}"
-            alt="Foto {{ $notification->data['user_registered']['name'] }}">
-        <div class="w-full truncate">
+        <div
+            class="flex items-center justify-center p-2 text-gray-400 bg-gray-200 rounded-full size-10 dark:text-zinc-300 dark:bg-zinc-700">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-handshake">
+                <path d="m11 17 2 2a1 1 0 1 0 3-3" />
+                <path
+                    d="m14 14 2.5 2.5a1 1 0 1 0 3-3l-3.88-3.88a3 3 0 0 0-4.24 0l-.88.88a1 1 0 1 1-3-3l2.81-2.81a5.79 5.79 0 0 1 7.06-.87l.47.28a2 2 0 0 0 1.42.25L21 4" />
+                <path d="m21 3 1 11h-2" />
+                <path d="M3 3 2 14l6.5 6.5a1 1 0 1 0 3-3" />
+                <path d="M3 4h8" />
+            </svg>
+        </div>
+        <div class="w-full">
             <div class="text-sm">
-                <a href="#" class="text-primary-400 hover:underline">
-                    {{ '@' . $notification->data['user_registered']['nick'] }}
-                </a>
-                <p class="truncate">
+                <p>
                     {{ $notification->data['message'] }}
                 </p>
             </div>
@@ -35,8 +42,7 @@
                         </div>
                     </button>
                 @endif
-                <a wire:navigate
-                    href="{{ route('find-player.show', ['id' => $notification->data['find_player']['id']]) }}"
+                <a wire:navigate href="#"
                     class="px-2 py-1 text-xs text-white rounded-lg bg-primary-600 hover:bg-primary-500">
                     visualizar
                 </a>
--- resources/views/components/notifications/type-notification.blade.php
+++ resources/views/components/notifications/type-notification.blade.php
@@ -1,13 +1,18 @@
 <div>
 
-  @switch($notification)
-  @case($notification->type === 'user-registered-vacancy')
-  <x-notifications.user-registered-vacancy-notification :notification="$notification" />
-  @break
-  @case($notification->type === 'user-accept-or-refuse-vacancy')
-  <x-notifications.user-accept-vacancy-notification :notification="$notification" />
-  @break
-  @default
+    @switch($notification)
+        @case($notification->type === 'user-registered-vacancy')
+            <x-notifications.user-registered-vacancy-notification :notification="$notification" />
+        @break
 
-  @endswitch
+        @case($notification->type === 'user-accept-or-refuse-vacancy')
+            <x-notifications.user-accept-vacancy-notification :notification="$notification" />
+        @break
+
+        @case($notification->type === 'friend-notification')
+            <x-notifications.friend-notification :notification="$notification" />
+        @break
+
+        @default
+    @endswitch
 </div>
