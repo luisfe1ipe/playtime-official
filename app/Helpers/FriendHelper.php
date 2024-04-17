c75b0100722e4cca1ff65bb5bf7f06ac29ceab9d
Luis Felipe <108816416+luisfe1ipe@users.noreply.github.com>
2024-04-17 10:04:29 -0300

    feat: criando helper para retornar apenas os usuários que não amigos

---
+++ app/Helpers/FriendHelper.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Helpers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Builder;
+
+class FriendHelper
+{
+  public static function getNotFriends(): Builder
+  {
+    $authId = Auth::user()->id;
+    $friendsIds = Auth::user()->friends->pluck('id');
+
+    $query = User::query();
+
+    return $query->whereNotIn('id', $friendsIds)
+      ->where('id', '!=', $authId);
+  }
+}
