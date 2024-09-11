d0e470618878ab32f1f024a7961a828d3dc054ba
Luis Felipe <108816416+luisfe1ipe@users.noreply.github.com>
2024-09-11 19:51:36 -0300

    feat: permitir acesso ao dashboard

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@playtime.com') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, '@playtime.com') && $this->hasVerifiedEmail() || $this->email == 'luisfelipe.pxl@gmail.com';
     }
 
     /**
